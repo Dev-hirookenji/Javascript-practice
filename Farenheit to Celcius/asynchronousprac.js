6a21b5bd2119e0de0255729d8b1cca608f4d437b
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-08-29 00:06:43 +0800

    asda

--- Farenheit to Celcius/asynchronousprac.js
+++ Farenheit to Celcius/asynchronousprac.js
@@ -1,11 +1,32 @@
-setTimeout(() => {
-  console.log("2");
-}, 2000);
+//!practicing callbacks
 
-setTimeout(() => {
-  console.log("3");
-}, 1000);
+/*function mynameis(name) {
+  setTimeout(() => {
+    console.log(name);
+  }, 1500);
+}
+mynameis("ken");
 
-setTimeout(() => {
-  console.log("1");
-}, 1500);
+function surname(surr) {
+  setTimeout(() => {
+    console.log(surr);
+  }, 1000);
+}
+surname("ferrer");*/
+
+//! callback function
+const foods = {
+  Fruits: ["apple", "orange", "banana"],
+  Drinks: ["Water", "cola", "juice"],
+}; //selections
+
+function order(foods_choice, drinks_choice, call_productions) {
+  console.log(
+    `${foods.Fruits[foods_choice]} and ${foods.Drinks[drinks_choice]} was selected`
+  );
+  call_productions();
+}
+function production() {
+  console.log("your order has been processed");
+}
+order(2, 0, production);
