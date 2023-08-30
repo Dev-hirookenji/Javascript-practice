e000b96301f01bdc509eccb1a9849854d38fdf35
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-08-30 23:47:02 +0800

    dad

--- Farenheit to Celcius/asynchronousprac.js
+++ Farenheit to Celcius/asynchronousprac.js
@@ -15,21 +15,28 @@ function surname(surr) {
 surname("ferrer");*/
 
 //! callback function
-const foods = {
-  Fruits: ["banana", "strawberry", "apple"],
-  Drinks: ["juice", "water", "cola"],
-};
+// const foods = {
+//   Fruits: ["banana", "strawberry", "apple"],
+//   Drinks: ["juice", "water", "cola"],
+// };
+// const cut = {
+//   Slice: ["Cut into 1 piece", "Cut into 2 pieces", "Cut into 3 pieces"],
+//   Dice: ["Dice into 1 piece", "Dice into 2 pieces", "Dice into 3 pieces"],
+// };
 
-function order(fruit_choice, drinks_choice, confirmation_done) {
-  console.log(
-    `The selected fruit is ${foods.Fruits[fruit_choice]} and the drink is ${foods.Drinks[drinks_choice]}`
-  );
-  confirmation_done();
-}
+// function order(fruit_choice, drinks_choice, cut_choice, confirmation_done) {
+//   console.log(
+//     `The selected fruit is ${foods.Fruits[fruit_choice]} and the drink is ${foods.Drinks[drinks_choice]}`
+//   );
+//   setTimeout(() => {
+//     console.log(`The chosen cut is to: ${cut.Slice[cut_choice]}`);
+//   }, 1000);
+//   confirmation_done();
+// }
 
-function confirmation() {
-  setTimeout(() => {
-    console.log("done");
-  }, 5000);
-}
-order(0, 2, confirmation);
+// function confirmation() {
+//   setTimeout(() => {
+//     console.log("done");
+//   }, 3000);
+// }
+// order(0, 2, 2, confirmation);
