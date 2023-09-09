27a68322acf5e0f9594fc15993dd943f80b508eb
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-09-09 22:05:10 +0800

    khjg

--- apipractice/apitest.js
+++ apipractice/apitest.js
@@ -1,10 +1,8 @@
+//async await
+const url = "https://api.genderize.io?name=scarlet";
+
 async function apicall() {
-  let response = await fetch("https://api.genderize.io?name=scarlet");
-  if (response.ok) {
-    console.log("nice");
-  } else {
-    console.log("bad");
-  }
+  let response = await fetch(url);
   let data = await response.json();
   console.log(data);
 }
