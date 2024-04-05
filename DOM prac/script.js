11873312cf84c27cd77a0c21289bffeff157fa80
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2024-04-06 04:11:44 +0800

    fucc

--- DOM prac/script.js
+++ DOM prac/script.js
@@ -3,20 +3,21 @@ app.innerHTML = `<ul id="lists">
 <li>"kiss"</li>
 <li>"bangus"</li>
 </ul>
+
 `;
 
-const div = document.createElement("div");
-app.innerHTML = `
-  <form>
-    <label>
-    Email
-    <input type="email">
-    </label></br>
-    <label>
-    Check me out
-    <input type="checkbox">
-    </label>
-  </form>`;
+// const div = document.createElement("div");
+// app.innerHTML = `
+//   <form>
+//     <label>
+//     Email
+//     <input type="email">
+//     </label></br>
+//     <label>
+//     Check me out
+//     <input type="checkbox">
+//     </label>
+//   </form>`;
 
 //creating forms using js dom
 
@@ -31,25 +32,26 @@ app.innerHTML = `
 // app.innerHTML += createdEmail;
 // const createdPassword = createForm({ label: "Password", type: "password" });
 // app.innerHTML += createdPassword;
-// //list
+//list
 
-// const data = ["Samsung A55", "Bag", "Sipag"];
-// const fragment = document.createDocumentFragment();
+const data = ["Samsung A55", "Bag", "Sipag"];
+const fragment = document.createDocumentFragment();
 
-// data.forEach((item) => {
-//   const li = document.createElement("li");
-//   li.className = "list-item";
-//   li.innerHTML = item;
-//   fragment.appendChild(li);
-// });
+data.forEach((item) => {
+  const li = document.createElement("li");
+  li.className = "list-item";
+  li.innerText = item;
+  fragment.append(li);
+});
 
-// const querySelect = document.querySelector("#lists");
-// querySelect.append(fragment);
+const selector = document.querySelector("#lists");
+selector.append(fragment);
 
-// const newItem = document.createElement("li");
-// newItem.className = "list-item";
-// newItem.innerText = "Good Item";
-// querySelect.append(newItem);
+const newItem = document.createElement("li");
+newItem.className = "list-item";
+newItem.innerText = "New Item";
+
+selector.append(newItem);
 
 // //!looping through
 // const list = document.querySelectorAll("#lists li");
@@ -68,14 +70,22 @@ app.innerHTML = `
 // //? addEventListener
 // button.addEventListener("click", handleclick);
 
-//!preventDefault();
-const form = document.querySelector("form");
-const email = form.querySelector('input[type="email"]');
-const checkbox = form.querySelector('input[type="checkbox"]');
+// //!preventDefault();
+// const form = document.querySelector("form");
+// const email = form.querySelector('input[type="email"]');
+// const checkbox = form.querySelector('input[type="checkbox"]');
 
-function submitHandler(event) {
-  event.preventDefault();
-  console.log(email.value, checkbox.checked);
-}
+// function submitHandler(event) {
+//   event.preventDefault();
+//   console.log(email.value, checkbox.checked);
+// }
 
-form.addEventListener("submit", submitHandler);
+// form.addEventListener("submit", submitHandler);
+
+// //! event delegation
+// const list = document.querySelector("#list");
+// const items = list.querySelectorAll("li");
+
+// function handleclick(event) {
+//   console.log(event.target.innerText);
+// }
